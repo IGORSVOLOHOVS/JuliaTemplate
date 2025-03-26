021e48d7ddf1864fdb5da8a73309b3205ce75c1c
Igors Volohovs <igorsvolohovs@gmail.com>
2025-03-26 11:02:57 +0200

    Update cover.jl

--- template/test/cover.jl
+++ template/test/cover.jl
@@ -1,1 +1,8 @@
 using Coverage
+
+coverage = process_folder() 
+coverage = append!(coverage, process_folder("test"))  
+coverage = append!(coverage, process_folder("docs"))  
+coverage = append!(coverage, process_folder("build")) 
+
+LCOV.writefile("coverage.info", coverage)
